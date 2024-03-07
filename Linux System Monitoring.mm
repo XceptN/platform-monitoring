4bb1da0188a65451c0f688621d5c2799400c9a87
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-03-07 14:01:54 +0300

    Start scripting

--- Linux System Monitoring.mm
+++ Linux System Monitoring.mm
@@ -80,7 +80,13 @@
 <node TEXT="Require env where pcp is available" POSITION="right" ID="ID_1258114593" CREATED="1705661857250" MODIFIED="1705661869517"/>
 <node TEXT="Configure pcp" POSITION="right" ID="ID_437397818" CREATED="1705661869946" MODIFIED="1705661874639"/>
 <node TEXT="Maybe configure with Grafana" POSITION="right" ID="ID_212149912" CREATED="1705661875089" MODIFIED="1705661884884"/>
-<node TEXT="Collect data in CSV format and send to vispeahen" POSITION="right" ID="ID_1206489466" CREATED="1705661885217" MODIFIED="1705661918857"/>
+<node TEXT="Collect data in CSV format and send to vispeahen" POSITION="right" ID="ID_1206489466" CREATED="1705661885217" MODIFIED="1705661918857">
+<node TEXT="vispeahen&apos;a gondermeden once PostgreSQL&apos;e atiliyormus veriler" ID="ID_208676702" CREATED="1709794673879" MODIFIED="1709794687449"/>
+<node TEXT="Selcuk toplanti ayarlayacak, kullandiklari yapiyi anlatacaklar" ID="ID_272462496" CREATED="1709794688548" MODIFIED="1709794704484"/>
+<node TEXT="veri postgresql&apos;e nasil yuklenecek" ID="ID_1807433466" CREATED="1709794710239" MODIFIED="1709794719984">
+<node TEXT="Tablolarin dinamik kolon olma sansi var mi ?" ID="ID_1929105842" CREATED="1709794723532" MODIFIED="1709794730878"/>
+</node>
+</node>
 <node TEXT="https://github.com/XceptN/work-track/issues/4" POSITION="left" ID="ID_1552310232" CREATED="1706088042188" MODIFIED="1706689166670" LINK="https://github.com/XceptN/work-track/issues/4"/>
 <node TEXT="PCP availability on various distros" POSITION="left" ID="ID_255219827" CREATED="1706690522622" MODIFIED="1706690532680">
 <node TEXT="All RHEL compatible ones" ID="ID_284427916" CREATED="1706690534631" MODIFIED="1706690541410"/>
@@ -89,21 +95,39 @@
 </node>
 <node TEXT="What to Log with PCP" POSITION="left" ID="ID_1289951742" CREATED="1706691168762" MODIFIED="1706691173715">
 <node TEXT="CPU" ID="ID_908336688" CREATED="1706691225346" MODIFIED="1706691226182">
+<node TEXT="overall" ID="ID_221053554" CREATED="1709794504119" MODIFIED="1709794505770">
 <node TEXT="kernel.cpu.util.user" ID="ID_637030391" CREATED="1706691211040" MODIFIED="1706691215791"/>
 <node TEXT="kernel.cpu.util.sys" ID="ID_510725285" CREATED="1706691211041" MODIFIED="1706691218190"/>
 <node TEXT="kernel.cpu.util.wait" ID="ID_1871221202" CREATED="1706691211043" MODIFIED="1706691220350"/>
 <node TEXT="kernel.cpu.util.idle" ID="ID_47052643" CREATED="1706691211044" MODIFIED="1706691222615"/>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.cpu.util.user kernel.cpu.util.sys kernel.cpu.util.wait kernel.cpu.util.idle" ID="ID_1432107161" CREATED="1709624018634" MODIFIED="1709645546301"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.cpu.util.user kernel.cpu.util.sys kernel.cpu.util.wait kernel.cpu.util.idle" ID="ID_1432107161" CREATED="1709624018634" MODIFIED="1709645546301">
+<node TEXT="cpu_general.csv" ID="ID_1405412865" CREATED="1709808633702" MODIFIED="1709808633702"/>
+</node>
+</node>
 <node TEXT="Multiprocessor distribution" ID="ID_350118055" CREATED="1709624124843" MODIFIED="1709624134335">
+<node TEXT="core sayisi kada veriseti olacak (nproc)" ID="ID_446154389" CREATED="1709794485146" MODIFIED="1709794522729">
+<node TEXT="nproc.csv" ID="ID_1131151183" CREATED="1709808810122" MODIFIED="1709808810122"/>
+</node>
 <node TEXT="Scale of 1000" ID="ID_1464683126" CREATED="1709626832380" MODIFIED="1709626837984"/>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.percpu.cpu.idle" ID="ID_432876852" CREATED="1709626435297" MODIFIED="1709645540941"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.percpu.cpu.idle" ID="ID_432876852" CREATED="1709626435297" MODIFIED="1709645540941">
+<node TEXT="multi_cpu.csv" ID="ID_864011812" CREATED="1709808756775" MODIFIED="1709808756775"/>
+</node>
+<node TEXT="tabloyu her biri bir core olacak sekilde ayirmak gerekebilir vispeahen icin" ID="ID_1533058423" CREATED="1709794563943" MODIFIED="1709794586112">
+<node TEXT="Ya da CPU kolonu ekleyip, veriyi ona gore duzenleyebiliriz .." ID="ID_969678085" CREATED="1709800835331" MODIFIED="1709800957337"/>
+<node TEXT="Bunu veriyi postgresql&apos;e atarken otomatize etsek?" ID="ID_700699941" CREATED="1709800862019" MODIFIED="1709800872706"/>
+<node TEXT="for i in XXX diyip ayri ayri tablolar create ederiz" ID="ID_27335499" CREATED="1709800911202" MODIFIED="1709800922884"/>
+<node TEXT="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres" ID="ID_1358446152" CREATED="1709808184542" MODIFIED="1709808184542" LINK="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres"/>
+</node>
 </node>
 <node TEXT="Loadavg" ID="ID_1856317363" CREATED="1706691392779" MODIFIED="1706691396310">
 <node TEXT="Compare with Core/Thread count" ID="ID_1338796" CREATED="1706691443621" MODIFIED="1706691454672">
 <node TEXT="how to get" ID="ID_1737622785" CREATED="1709626928031" MODIFIED="1709626929183"/>
 <node TEXT="/usr/bin/nproc" ID="ID_881593683" CREATED="1709626984929" MODIFIED="1709626988264"/>
+<node TEXT="nproc.csv" ID="ID_1599225938" CREATED="1709808812918" MODIFIED="1709808812918"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.all.load" ID="ID_124491159" CREATED="1709624018634" MODIFIED="1709735969826"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; kernel.all.load" ID="ID_124491159" CREATED="1709624018634" MODIFIED="1709735969826">
+<node TEXT="loadavg.csv" ID="ID_355576734" CREATED="1709808735666" MODIFIED="1709808735666"/>
+</node>
 </node>
 </node>
 <node TEXT="Memory" ID="ID_1319295949" CREATED="1706691245771" MODIFIED="1706691247471">
@@ -137,7 +161,9 @@
 <node TEXT="mem.util.swapTotal" ID="ID_582769451" CREATED="1709627322030" MODIFIED="1709627322030"/>
 <node TEXT="mem.util.swapFree" ID="ID_649602348" CREATED="1709627329334" MODIFIED="1709627329334"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; mem.physmem mem.util.free mem.util.available mem.util.pageTables mem.util.bufmem mem.util.cached mem.util.slab mem.util.swapTotal mem.util.swapFree" ID="ID_1174203050" CREATED="1709624018634" MODIFIED="1709645533289"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; mem.physmem mem.util.free mem.util.available mem.util.pageTables mem.util.bufmem mem.util.cached mem.util.slab mem.util.swapTotal mem.util.swapFree" ID="ID_1174203050" CREATED="1709624018634" MODIFIED="1709645533289">
+<node TEXT="memory.csv" ID="ID_320936034" CREATED="1709808746712" MODIFIED="1709808746712"/>
+</node>
 </node>
 <node TEXT="SLAB/SLUB" FOLDED="true" ID="ID_1707196138" CREATED="1706691295368" MODIFIED="1706691307845">
 <node TEXT="are these really needed?" ID="ID_284496462" CREATED="1706694632525" MODIFIED="1706694637431"/>
@@ -152,7 +178,9 @@
 <node TEXT="filesys.freefiles" ID="ID_1101979666" CREATED="1709638242072" MODIFIED="1709638251984">
 <node TEXT="count" ID="ID_744434573" CREATED="1709638656084" MODIFIED="1709638656875"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics  --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; filesys.free filesys.freefiles" ID="ID_1144731459" CREATED="1709638690038" MODIFIED="1709645525883"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics  --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; filesys.free filesys.freefiles" ID="ID_1144731459" CREATED="1709638690038" MODIFIED="1709645525883">
+<node TEXT="diskfree.csv" ID="ID_71021109" CREATED="1709808657585" MODIFIED="1709808657585"/>
+</node>
 </node>
 <node TEXT="I/O rates" ID="ID_525417360" CREATED="1706694702894" MODIFIED="1706694711072">
 <node TEXT="overall" ID="ID_1181173310" CREATED="1709638028343" MODIFIED="1709638029967">
@@ -168,7 +196,9 @@
 <node TEXT="w b/s" ID="ID_1548747837" CREATED="1706694779890" MODIFIED="1709637865043">
 <node TEXT="disk.all.write_bytes" ID="ID_1142458072" CREATED="1709637903579" MODIFIED="1709637903579"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics  --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; disk.all.read disk.all.write disk.all.read_bytes disk.all.write_bytes" ID="ID_1477183636" CREATED="1709640324007" MODIFIED="1709645517558"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics  --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; disk.all.read disk.all.write disk.all.read_bytes disk.all.write_bytes" ID="ID_1477183636" CREATED="1709640324007" MODIFIED="1709645517558">
+<node TEXT="io_rates_overall.csv" ID="ID_75926086" CREATED="1709808675008" MODIFIED="1709808675008"/>
+</node>
 </node>
 <node TEXT="per-device" ID="ID_604536652" CREATED="1709638033047" MODIFIED="1709638034939">
 <node TEXT="r/s" ID="ID_356802271" CREATED="1706694746185" MODIFIED="1706694749138">
@@ -189,7 +219,15 @@
 <node TEXT="%util" ID="ID_749735079" CREATED="1706694782020" MODIFIED="1706694788850">
 <node TEXT="disk.dev.util" ID="ID_1656846497" CREATED="1709638147434" MODIFIED="1709638149806"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; disk.dev.read disk.dev.write disk.dev.read_bytes disk.dev.write_bytes disk.dev.avg_qlen disk.dev.util" ID="ID_1475836223" CREATED="1709641434974" MODIFIED="1709645513371"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; disk.dev.read disk.dev.write disk.dev.read_bytes disk.dev.write_bytes disk.dev.avg_qlen disk.dev.util" ID="ID_1475836223" CREATED="1709641434974" MODIFIED="1709645513371">
+<node TEXT="io_rates_perdevice.csv" ID="ID_1798294052" CREATED="1709808723924" MODIFIED="1709808723924"/>
+</node>
+<node TEXT="tabloyu her biri bir device olacak sekilde ayirmak gerekebilir vispeahen icin" ID="ID_1163717600" CREATED="1709794563943" MODIFIED="1709808697493">
+<node TEXT="Ya da device kolonu ekleyip, veriyi ona gore duzenleyebiliriz .." ID="ID_486362125" CREATED="1709800835331" MODIFIED="1709808713668"/>
+<node TEXT="Bunu veriyi postgresql&apos;e atarken otomatize etsek?" ID="ID_1211890686" CREATED="1709800862019" MODIFIED="1709800872706"/>
+<node TEXT="for i in XXX diyip ayri ayri tablolar create ederiz" ID="ID_1786984642" CREATED="1709800911202" MODIFIED="1709800922884"/>
+<node TEXT="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres" ID="ID_1665725428" CREATED="1709808184542" MODIFIED="1709808184542" LINK="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres"/>
+</node>
 </node>
 </node>
 </node>
@@ -233,11 +271,25 @@
 </node>
 </node>
 <node TEXT="Per interface" ID="ID_1990051962" CREATED="1709647551547" MODIFIED="1709647555082">
+<node TEXT="Burada interface sayisi kadar veri seti olacak" ID="ID_1171506302" CREATED="1709794398433" MODIFIED="1709794408937"/>
+<node TEXT="interface listesi" ID="ID_788916360" CREATED="1709794810587" MODIFIED="1709794813770">
+<node TEXT="ip -o link show | awk -F: &apos;{ print $2 }&apos; | sed &apos;s/ //&apos;" ID="ID_1843512161" CREATED="1709797335132" MODIFIED="1709797335132"/>
+<node TEXT="ip -o link show | sed &apos;s/[[:digit:]]+: \([[:alnum:]]+\):.*/\1/&apos;" ID="ID_570355025" CREATED="1709797728402" MODIFIED="1709797748219"/>
+</node>
+<node TEXT="vispeahen icin tablolari her biri bir interface icin olacak sekilde ayirmak gerekebilir" ID="ID_155975504" CREATED="1709794608830" MODIFIED="1709794630728">
+<node TEXT="cunku tablo kolon sayilari dinamik olmayacak" ID="ID_1710092474" CREATED="1709794632612" MODIFIED="1709794642048"/>
+<node TEXT="Ya da network interface kolonu ekleyip, veriyi ona gore duzenleyebiliriz .." ID="ID_1251713495" CREATED="1709800835331" MODIFIED="1709800861303"/>
+<node TEXT="Bunu veriyi postgresql&apos;e atarken otomatize etsek?" ID="ID_425782024" CREATED="1709800862019" MODIFIED="1709800872706"/>
+<node TEXT="for i in XXX diyip ayri ayri tablolar create ederiz" ID="ID_1089932500" CREATED="1709800911202" MODIFIED="1709800922884"/>
+<node TEXT="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres" ID="ID_163464375" CREATED="1709808170964" MODIFIED="1709808170964" LINK="https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres"/>
+</node>
 <node TEXT="Bandwidth Usage" ID="ID_1247100018" CREATED="1706691346000" MODIFIED="1706691351487">
 <node TEXT="network.interface.in.bytes" ID="ID_1626077276" CREATED="1709647790902" MODIFIED="1709647790902"/>
 <node TEXT="network.interface.out.bytes" ID="ID_692031094" CREATED="1709647803471" MODIFIED="1709647803471"/>
 <node TEXT="network.interface.total.bytes" ID="ID_1039104421" CREATED="1709647822604" MODIFIED="1709647822604"/>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; network.interface.in.bytes network.interface.out.bytes network.interface.total.bytes" ID="ID_356179703" CREATED="1709733899886" MODIFIED="1709733899886"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; network.interface.in.bytes network.interface.out.bytes network.interface.total.bytes" ID="ID_356179703" CREATED="1709733899886" MODIFIED="1709733899886">
+<node TEXT="network_bandwidth_usage.csv" ID="ID_508667040" CREATED="1709808774406" MODIFIED="1709808774406"/>
+</node>
 </node>
 <node TEXT="Error percentage" ID="ID_1169983318" CREATED="1706691352515" MODIFIED="1706691361576">
 <node TEXT="in" ID="ID_224906322" CREATED="1709647790028" MODIFIED="1709647790390">
@@ -255,7 +307,9 @@
 <node TEXT="network.interface.total.errors" ID="ID_1148727677" CREATED="1709647822605" MODIFIED="1709647822605"/>
 <node TEXT="network.interface.total.drops" ID="ID_956447621" CREATED="1709647822605" MODIFIED="1709647822605"/>
 </node>
-<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; network.interface.in.packets network.interface.in.errors network.interface.in.drops network.interface.out.packets network.interface.out.errors network.interface.out.drops network.interface.total.packets network.interface.total.errors network.interface.total.drops" ID="ID_1438206519" CREATED="1709734682357" MODIFIED="1709734682357"/>
+<node TEXT="pmdumptext --start=&apos;2 days ago&apos; --interval=1hour --archive=ocitest01 --align=1day --metrics --unit --delimiter=, --time-format=&apos;%Y-%m-%d %H:%M&apos; network.interface.in.packets network.interface.in.errors network.interface.in.drops network.interface.out.packets network.interface.out.errors network.interface.out.drops network.interface.total.packets network.interface.total.errors network.interface.total.drops" ID="ID_1438206519" CREATED="1709734682357" MODIFIED="1709734682357">
+<node TEXT="network_error_rates.csv" ID="ID_1498532309" CREATED="1709808784771" MODIFIED="1709808784771"/>
+</node>
 </node>
 </node>
 </node>
