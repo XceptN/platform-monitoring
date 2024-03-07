31186aee91b932da1302b312ba6b1699335a9803
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-03-07 15:21:53 +0300

    CPU general

--- collect-data-into-csv.sh
+++ collect-data-into-csv.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+TMPDIR=/tmp/$RANDOM
+
+mkdir $TMPDIR
+
+PARCHDIR=$(pcp | grep "pmlogger: primary logger: " | cut -c 28- | sed "s/\(.*\)$HOSTNAME.*/\1/")
+PSTART='yesterday'
+PINTERVAL='1hour'
+PHOST=$HOSTNAME
+PALIGN='1day'
+PTIMEFMT='%Y-%m-%d,%H:%M'
+PCMDPRE="pmdumptext --start=$PSTART --interval=$PINTERVAL --archive=$PHOST --align=$PALIGN --metrics --unit --delimiter=, --time-format=$PTIMEFMT"
+
+cd $PARCHDIR
+
+# CPU General
+$PCMDPRE \
+    kernel.cpu.util.user \
+    kernel.cpu.util.sys \
+    kernel.cpu.util.wait \
+    kernel.cpu.util.idle \
+    | sed 's/^Time/Date,Time/' \
+    | sed 's/none,none,none,none,none/none,none,%,%,%,%/' \
+    > $TMPDIR/cpu_general.csv
+# Maybe also do something about ?,?,?,?
+
