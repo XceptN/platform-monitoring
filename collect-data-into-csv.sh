449d3916a834b199c449a251d2c0d826f0e36e21
Ozgur Yuksel <ozgur.yuksel@bisoft.com.tr>
2024-03-07 17:03:32 +0300

    Multi CPU metrics processed and finalized

--- collect-data-into-csv.sh
+++ collect-data-into-csv.sh
@@ -2,26 +2,44 @@
 
 TMPDIR=/tmp/$RANDOM
 
-mkdir $TMPDIR
+mkdir -p $TMPDIR/final
 
-PARCHDIR=$(pcp | grep "pmlogger: primary logger: " | cut -c 28- | sed "s/\(.*\)$HOSTNAME.*/\1/")
+#PARCHDIR=$(pcp | grep "pmlogger: primary logger: " | cut -c 28- | sed "s/\(.*\)$HOSTNAME.*/\1/"`
+PARCHDIR='/var/log/pcp/pmlogger'
 PSTART='yesterday'
 PINTERVAL='1hour'
 PHOST=$HOSTNAME
 PALIGN='1day'
 PTIMEFMT='%Y-%m-%d,%H:%M'
-PCMDPRE="pmdumptext --start=$PSTART --interval=$PINTERVAL --archive=$PHOST --align=$PALIGN --metrics --unit --delimiter=, --time-format=$PTIMEFMT"
+PCMD="pmdumptext --start=$PSTART --interval=$PINTERVAL --archive=$PHOST --align=$PALIGN --metrics --unit --delimiter=, --time-format=$PTIMEFMT"
 
 cd $PARCHDIR
 
 # CPU General
-$PCMDPRE \
-    kernel.cpu.util.user \
-    kernel.cpu.util.sys \
-    kernel.cpu.util.wait \
-    kernel.cpu.util.idle \
-    | sed 's/^Time/Date,Time/' \
-    | sed 's/none,none,none,none,none/none,none,%,%,%,%/' \
-    > $TMPDIR/cpu_general.csv
-# Maybe also do something about ?,?,?,?
+#$PCMD \
+#    kernel.cpu.util.user \
+#    kernel.cpu.util.sys \
+#    kernel.cpu.util.wait \
+#    kernel.cpu.util.idle \
+#    | grep -v '?' \
+#    | sed 's/^Time/Date,Time/' \
+#    | sed 's/^none,none,none,none,none/none,none,%,%,%,%/' \
+#    > $TMPDIR/final/cpu_general.csv
 
+# Multi CPU stats
+NPROC=$(nproc)
+$PCMD kernel.percpu.cpu.idle \
+    | grep -v '?' \
+    | tail +3 \
+    > $TMPDIR/multi_cpu_raw.csv
+
+echo "Date,Time,CPU,Busy" > $TMPDIR/final/multi_cpu.csv
+echo "none,none,string,%" >> $TMPDIR/final/multi_cpu.csv
+
+for ((CPU=0; CPU<$NPROC; CPU++))
+do 
+    PRINCOL=$((3+$CPU*1))
+#    echo "CPU=$CPU"
+#    echo "PRINCOL=$PRINCOL"
+    awk -F, '{ printf "%s,%s,cpu'"$CPU"',%.2f\n", $1, $2, (1000-$'"$PRINCOL"')/10 }' $TMPDIR/multi_cpu_raw.csv >> $TMPDIR/final/multi_cpu.csv
+done
